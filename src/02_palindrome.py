e4039e6a7bd2c148354f28dd69f313d19ac2bacd
Agustín Arturo Melian Su <142176619+aguamelsu@users.noreply.github.com>
2025-07-27 03:26:45 +0000

    Added comments and reviewed the work of members 1 and 2

--- src/02_palindrome.py
+++ src/02_palindrome.py
@@ -1,2 +1,3 @@
-def is_palindrome(word):
+# This function expects a str and returns a str
+def is_palindrome(word: str) -> str:
     return ("is palindrome" if (word[::-1]==word) else "is not palindrome")
